688d18e71b6cfbfcecf03ca1a2517ebac49f5af5
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-06-22 23:47:21 +0900

    マークアップ、クラス付与

--- fire/src/App.tsx
+++ fire/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App relative flex h-screen items-center justify-center">
+      <header className="l-header">
+        <img src={logo} className="l-header__logo w-24" alt="logo" />
       </header>
+
+      <section className="absolute mx-auto h-2/3 w-96 rounded-md bg-white p-10">
+        <h1 className="absolute -bottom-20 -right-24 text-6xl text-white">
+          File Fire
+        </h1>
+        <div className="l-form-area">
+          <div className="flex flex-wrap justify-center">
+            <div className="l-upload mb-24 h-32 w-32 border-2 border-dotted border-gray-400"></div>
+            <div className="mb-12 w-full">
+              <input
+                name="file"
+                type="file"
+                accept=""
+                className="c-button01 border-none"
+              />
+            </div>
+            <input
+              type="button"
+              disabled={true}
+              value="アップする"
+              className="c-button02 text-md h-12 w-36"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
