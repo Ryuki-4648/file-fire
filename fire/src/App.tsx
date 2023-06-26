0e2dbbacbd9161541b19aa3d43b756b4a671870b
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-06-26 21:01:47 +0900

    インデント、alt、ヘッダー位置

--- fire/src/App.tsx
+++ fire/src/App.tsx
@@ -13,7 +13,9 @@ function App() {
   return (
     <div className="App relative flex h-screen items-center justify-center overflow-hidden">
       <header className="l-header">
-        <h1>File Fire</h1>
+        <h1 className="absolute left-4 top-4 w-full text-xl tracking-widest text-white">
+          File Fire
+        </h1>
       </header>
       <img
         src="/img_title01.png"
@@ -21,71 +23,69 @@ function App() {
         alt="タイトル"
       />
       <section className="c-box01 absolute mx-auto h-2/3 w-4/5 rounded-md bg-white px-6 py-12 md:w-96">
-        <div className="">
-          <p className="c-title01 absolute -top-6 left-0 mb-10 text-center text-xl font-bold tracking-wider text-white md:-top-7 md:text-2xl">
-            忘れたい過去をお焚き上げ
-          </p>
-          <div className="flex flex-wrap justify-center">
-            {uploadedFile ? (
-              <>
-                <div
-                  className={
-                    isFinished ? "hidden" : "flex flex-wrap justify-center"
-                  }
-                >
-                  <img
-                    src="/img_file01.png"
-                    alt=""
-                    className="image02 absolute top-12 z-10 w-32"
-                  />
-                  <img
-                    src="/img_fire01.png"
-                    alt=""
-                    className="image01 z-1 absolute top-32 w-40"
-                  />
-                </div>
-              </>
-            ) : (
-              <div
-                {...getRootProps()}
-                className="l-upload-box mb-12 flex h-44 w-44 cursor-pointer flex-wrap items-center justify-center border-2 border-dotted border-gray-400 p-4 hover:border-gray-800"
-              >
-                <input {...getInputProps()} />
-                {isDragActive ? (
-                  <p className="text-center text-sm">アップ中...</p>
-                ) : (
-                  <p className="text-center text-sm leading-relaxed">
-                    ファイルをドロップ
-                    <br />
-                    または
-                    <br />
-                    クリックして選択
-                  </p>
-                )}
-              </div>
-            )}
-            {uploadedFile && (
+        <p className="c-title01 absolute -top-6 left-0 mb-10 text-center text-xl font-bold tracking-wider text-white md:-top-7 md:text-2xl">
+          忘れたい過去をお焚き上げ
+        </p>
+        <div className="flex flex-wrap justify-center">
+          {uploadedFile ? (
+            <>
               <div
                 className={
-                  isFinished
-                    ? "hidden"
-                    : "absolute bottom-16 mb-6 flex w-full justify-center"
+                  isFinished ? "hidden" : "flex flex-wrap justify-center"
                 }
               >
-                <p className="mr-2 text-xs md:text-sm">あなたの忘れたいもの</p>
-                <p className="text-center text-xs md:text-sm">
-                  {uploadedFile.name.split(".")[0]}
-                </p>
+                <img
+                  src="/img_file01.png"
+                  alt="ファイル"
+                  className="image02 absolute top-12 z-10 w-32"
+                />
+                <img
+                  src="/img_fire01.png"
+                  alt="焚き火"
+                  className="image01 z-1 absolute top-32 w-40"
+                />
               </div>
-            )}
-            <p
-              className="c-reset-button text-md absolute bottom-12 w-full cursor-pointer text-center text-blue-500 underline duration-300 hover:text-blue-900"
-              onClick={resetFile}
+            </>
+          ) : (
+            <div
+              {...getRootProps()}
+              className="l-upload-box mb-12 flex h-44 w-44 cursor-pointer flex-wrap items-center justify-center border-2 border-dotted border-gray-400 p-4 hover:border-gray-800"
             >
-              焚きなおす
-            </p>
-            {isFinished && <img src="/img_fire02.png" alt="燃えたあと" />}
-          </div>
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p className="text-center text-sm">アップ中...</p>
+              ) : (
+                <p className="text-center text-sm leading-relaxed">
+                  ファイルをドロップ
+                  <br />
+                  または
+                  <br />
+                  クリックして選択
+                </p>
+              )}
+            </div>
+          )}
+          {uploadedFile && (
+            <div
+              className={
+                isFinished
+                  ? "hidden"
+                  : "absolute bottom-16 mb-6 flex w-full justify-center"
+              }
+            >
+              <p className="mr-2 text-xs md:text-sm">あなたの忘れたいもの</p>
+              <p className="text-center text-xs md:text-sm">
+                {uploadedFile.name.split(".")[0]}
+              </p>
+            </div>
+          )}
+          <p
+            className="c-reset-button text-md absolute bottom-12 w-full cursor-pointer text-center text-blue-500 underline duration-300 hover:text-blue-900"
+            onClick={resetFile}
+          >
+            焚きなおす
+          </p>
+          {isFinished && <img src="/img_fire02.png" alt="燃えたあとの焚き火" />}
         </div>
       </section>
     </div>
